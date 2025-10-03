390a1ef3b9fbdc679d1484bb453f7476fb4cc469
AnagaShriK <154398617+AnagaShriK@users.noreply.github.com>
2025-10-03 23:36:24 +0530

    Refactor NavBar to use React Bootstrap components

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,49 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../store/slices/userSlice';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
-function NavBar() {
-  const dispatch = useDispatch();
-  const { userInfo } = useSelector(state => state.user);
-
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-
+function NavBar({ userInfo, handleLogout }) {
   return (
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container">
-    <Link className="navbar-brand text-success" to="/">GreenGoods</Link>
-
-    {/* Toggler for mobile */}
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
-        {userInfo ? (
-          <>
-            <li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/orders">Orders</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/profile">{userInfo.name}</Link></li>
-            <li className="nav-item">
-              <button className="btn btn-link nav-link" onClick={handleLogout}>Logout</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-          </>
-        )}
-      </ul>
-    </div>
-  </div>
-</nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>GreenGoods</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {userInfo ? (
+              <>
+                <LinkContainer to="/search"><Nav.Link>Search</Nav.Link></LinkContainer>
+                <LinkContainer to="/cart"><Nav.Link>Cart</Nav.Link></LinkContainer>
+                <LinkContainer to="/orders"><Nav.Link>Orders</Nav.Link></LinkContainer>
+                <LinkContainer to="/profile"><Nav.Link>{userInfo.name}</Nav.Link></LinkContainer>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/login"><Nav.Link>Login</Nav.Link></LinkContainer>
+                <LinkContainer to="/register"><Nav.Link>Register</Nav.Link></LinkContainer>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
 
 export default NavBar;
