a6f6a97e242ab0d66eb70983389132f1aece6744
AnagaShriK <154398617+AnagaShriK@users.noreply.github.com>
2025-10-03 23:26:39 +0530

    Refactor NavBar component for mobile responsiveness

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -12,32 +12,37 @@ function NavBar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand text-success" to="/">GreenGoods</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ms-auto">
-            {userInfo ? (
-              <>
-                <li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/orders">Orders</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/profile">{userInfo.name}</Link></li>
-                <li className="nav-item">
-                  <button className="btn btn-link nav-link" onClick={handleLogout}>Logout</button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-                <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-              </>
-            )}
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container">
+    <Link className="navbar-brand text-success" to="/">GreenGoods</Link>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
+    {/* Toggler for mobile */}
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        {userInfo ? (
+          <>
+            <li className="nav-item"><Link className="nav-link" to="/search">Search</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/cart">Cart</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/orders">Orders</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/profile">{userInfo.name}</Link></li>
+            <li className="nav-item">
+              <button className="btn btn-link nav-link" onClick={handleLogout}>Logout</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+          </>
+        )}
+      </ul>
+    </div>
+  </div>
+</nav>
   );
 }
 
